819bb7888725fde83f33af5c1aa472d0ee5213b1
Jake Westerfield <Jacob_Westerfield@discovery.com>
2022-03-28 17:24:57 -0400

    adds in index.js code and new config details

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 const request = require("request");
 
+const config = require("config");
+const gsheets_URL1 = config.get("google.google_url1");
+const spreadsheet_id = config.get("google.spreadsheet_id");
+const gsheets_URL2 = config.get("google.google_url2");
+const API_Key = config.get("google.gcpApp_APIKey");
+
+console.log(`Our key: ${key}`);
+
+
 exports.handler = function(event, context, callback) {
-  const key = "YOUR-KEY-HERE";
-  const url = "https://spreadsheets.google.com/feeds/list/"+key+"/od6/public/values?alt=json";
+    const key = key;
+    const url = gsheets_URL1+
+        spreadsheet_id+
+        gsheets_URL2+
+        API_Key;
 
-  request({
-    json: true,
-    url: url
-  }, function (error, response, body) {
-    if (error || response.statusCode !== 200) return
+    request({
+        json: true,
+        url: url
+    }, function (error, response, body) {
+        if (error || response.statusCode !== 200) return
 
-    let parsed = body.feed.entry.map( (entry) => {
-      let columns = {
-        "updated": entry.updated["$t"]
-      }
-
-      // Dynamically add all relevant columns from the Sheets to the response
-      Object.keys( entry ).forEach( (key) => {
-        if ( /gsx\$/.test(key) ) {
-          let newKey = key.replace("gsx$", "");
-          columns[newKey] = entry[key]["$t"];
+        let parsed = body.feed.entry.map( (entry) => {
+        let columns = {
+            "updated": entry.updated["$t"]
         }
-      });
 
-      return columns;
-    })
+        // Dynamically add all relevant columns from the Sheets to the response
+        Object.keys( entry ).forEach( (key) => {
+            if ( /gsx\$/.test(key) ) {
+            let newKey = key.replace("gsx$", "");
+            columns[newKey] = entry[key]["$t"];
+            }
+        });
 
-    callback(null, parsed);
-  });
+        return columns;
+        })
+
+        callback(null, parsed);
+    });
 };
